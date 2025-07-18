510d47092bef25b41447b8021a62c44d642cd0fb
aoukrif <abderaoufoukrif@gmail.com>
2025-07-18 10:08:49 +0100

    Add Holidays app with expense tracking and random chooser
    Added a `.gitignore` to exclude unnecessary files. Created a new
    Visual Studio solution (`HolidayApp.sln`) and project (`Holidays.csproj`)
    targeting .NET 8.0. Implemented a `Person` class to manage user
    spending and money transfers. Developed the main program logic
    in `Program.cs` to support random person selection and expense
    balancing functionality.

---
+++ Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChoser
+{
+    internal class Person
+    {
+        public static double totalSpent; 
+
+        public string Name;
+        public double MoneySpent=0;
+        public double diffrenceSpent = 0;
+        public void setName(string name)
+        {
+            this.Name = name;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public Person(string name)
+        {
+            this.Name = name;
+            
+        }
+        public void spent (double coast)
+        {
+            this.MoneySpent += coast;
+            totalSpent+=coast;
+        }
+        public string sendMoney(Person receiver)
+        {
+            string result; 
+
+            if (this.diffrenceSpent >= receiver.diffrenceSpent)
+            {
+                result = $"{this.Name} must send {receiver.diffrenceSpent} to  {receiver.Name}";
+                this.diffrenceSpent -= receiver.diffrenceSpent;
+                receiver.diffrenceSpent = 0;
+                return result;
+
+            }
+            if (this.diffrenceSpent < receiver.diffrenceSpent)
+            {
+                result = $"{this.Name} must send {this.diffrenceSpent} euro to  {receiver.Name}";
+                receiver.diffrenceSpent -= this.diffrenceSpent;
+                this.diffrenceSpent = 0;
+                return result;
+            }
+            return "error ";
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+using RandomChoser;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("you want to : \n " +
+            "1 - Random person choice \n " +
+            "2 - Calculate total spent with the tricount ");
+        int choice = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the names of the persons in the trip  ");
+        string names = Console.ReadLine();
+        string[] personsNames = names.Split(' ');
+
+        switch (choice)
+        {
+            case 1:
+                Random random = new Random();
+                int randomIndex = random.Next(0, personsNames.Length);
+                Console.WriteLine($" The chosen is {personsNames[randomIndex]} ");
+                break;
+
+            case 2:
+                Dictionary<string, Person> NamesPersonsDict = new Dictionary<string, Person>();
+                int i = 0;
+                foreach (string person in personsNames)
+                {
+                    Person personObj = new Person(personsNames[i]);
+                    NamesPersonsDict.Add(personsNames[i], personObj);
+                    i++;
+
+                }
+                bool again = true;
+                while (again)
+                {
+                    Console.WriteLine("enter a name spent");
+                    string name = Console.ReadLine();
+                    Console.WriteLine(" enter the coast ");
+                    double coast = double.Parse(Console.ReadLine());
+                    try
+                    {
+                        NamesPersonsDict[name].spent(coast);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("please entre a person name from the list");
+                        continue;
+                    }
+                    Console.WriteLine(" you want to add an other spent ? (y/n)");
+                    string againBis = Console.ReadLine();
+                    if (againBis == "n") { again = false; }
+                }
+                Console.WriteLine($"the total spent is {Person.totalSpent} ");
+                double averageSpent = Person.totalSpent / NamesPersonsDict.Count;
+
+                List<Person> sendersList = new List<Person>(); // only person with diffrence != 0
+                List<Person> receiverList = new List<Person>();
+
+                foreach (Person person in NamesPersonsDict.Values)
+                {
+                    person.diffrenceSpent = person.MoneySpent - averageSpent;
+                    if (person.diffrenceSpent > 0) { receiverList.Add(person); }
+                    if (person.diffrenceSpent < 0)
+                    {
+                        person.diffrenceSpent = -1 * person.diffrenceSpent;
+                        sendersList.Add(person);
+                    }
+                }
+                
+                foreach (Person sender in sendersList)
+                {
+                    foreach (Person receiver in receiverList)
+                    {
+                        Console.WriteLine(sender.sendMoney(receiver));
+
+                    }
+                }
+                break; 
+                
+
+                
+        }
+    }
+}
